719e8ccf5057382abc26366702499679cf542306
Felipe-Barahona <david.barahona.mt@usa.edu.co>
2022-10-17 20:31:46 -0500

    inciando un nuevo proyecto

---
+++ src/main/java/com/mintic/usa/NuevoCinema/NuevoCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.mintic.usa.NuevoCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuevoCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuevoCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mintic/usa/NuevoCinema/NuevoCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintic.usa.NuevoCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuevoCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
